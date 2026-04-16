f19ac3a438e0a49ad887d4863d60e9ea84e6f456
felipesfl <felipeferreira.desenvolvedor@gmail.com>
2026-04-16 11:10:11 -0300

    animacoes nao section 02 e nas da avaliacoes ,  slider infinito  - html e css,  e slide bota o - js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,38 @@ function nextImage() {
 
 }
 
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const wrapper = document.querySelector('.cards-wrapper');
+    const btnLeft = document.getElementById('btn-left');
+    const btnRight = document.getElementById('btn-right');
+
+    let scrollAmount = 0;
+    const step = 400;
+
+    function pararAnimacao() {
+        wrapper.style.animation = 'none';
+    }
+
+    function voltarAnimacao() {
+        setTimeout(() => {
+            wrapper.style.animation = 'scrollCards 60s linear infinite';
+        }, 2000);
+    }
+
+    btnRight.addEventListener('click', () => {
+        pararAnimacao();
+        scrollAmount -= step;
+        wrapper.style.transform = `translateX(${scrollAmount}px)`;
+        voltarAnimacao();
+    });
+
+    btnLeft.addEventListener('click', () => {
+        pararAnimacao();
+        scrollAmount += step;
+        wrapper.style.transform = `translateX(${scrollAmount}px)`;
+        voltarAnimacao();
+    });
+
+});
